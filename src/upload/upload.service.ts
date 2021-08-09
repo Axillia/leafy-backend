d77d16ca787c7aac9802e331774db0bcac16abeb
Kushan Shamika <shamikakushan@gmail.com>
2021-08-09 15:07:54 +0530

    provide aws region

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -10,6 +10,7 @@ export class UploadService {
   s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'ap-southeast-1',
   });
 
   upload = multer({
